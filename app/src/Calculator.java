fd44c8e575a5c67b7eb2faf7ea0a0ebd0508aa39
alber <alberto.pizzi@ing.austral.edu.ar>
2023-03-23 12:09:13 -0300

    initial commit

---
+++ app/src/Calculator.java
@@ -0,0 +1,9 @@
+public interface Calculator {
+
+    String sum(String a, String b);
+    String sub(String a, String b);
+    String mult(String a, String b);
+    String div(String a, String b);
+    String toHex(String binary);
+    String fromHex(String hex);
+}
